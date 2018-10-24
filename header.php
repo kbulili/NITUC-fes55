f21035a40db7b2eae39ebec95297dc9c6d5d9cc6
kbulili <crispyclone02@gmail.com>
2018-10-24 22:20:25 +0900

    メインページのデザインが終了しました①

---
+++ header.php
@@ -0,0 +1,9 @@
+<a href="index.html" style="text-decoration:none;" class="title">NITUC-fes #55</a>
+<ul>
+	<li>リンク</li>
+	<li>アクセス</li>
+	<li>バザー</li>
+	<li>タイムテーブル</li>
+	<li>ご挨拶</li>
+	<li>ホーム</li>
+</ul>
